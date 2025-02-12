0ef8c1201afcb4dffd73248c75b038f54010e38c
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-11 20:19:38 -0600

    understanding how to use multiple generics in a function. :)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
-fn identity<T>(value: T) -> T {
-    value
+fn make_tuple<T, U>(first: T, second: U) -> (T, U) {
+    (first, second)
 }
 
-#[derive(Debug)]
-struct Delisandwhich {}
-
 fn main() {
-    println!("{}", identity::<i32>(5));
-    println!("{}", identity::<i8>(5));
-    println!("{}", identity(true));
-    println!("{}", identity(String::from("Tacobell")));
-    println!("{}", identity(2.234));
-    println!("{:?}", identity(Delisandwhich {}));
+    make_tuple(String::from("Tacobell"), String::from("Banana"));
 }
