8c7bd997108730f74cba8112cc78cfef6f27e6c3
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-11 19:25:43 -0600

    understanding what generics are and how they can make our code much more efficient, easy to read, and clean. :)

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+fn identity<T>(value: T) -> T {
+    value
+}
+
+#[derive(Debug)]
+struct Delisandwhich {}
+
+fn main() {
+    println!("{}", identity(5));
+    println!("{}", identity(true));
+    println!("{}", identity(String::from("Tacobell")));
+    println!("{}", identity(2.234));
+    println!("{:?}", identity(Delisandwhich {}));
+}
