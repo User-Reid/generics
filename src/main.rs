35a2b23d12d0d6ea61b2039a0df7213ad5efcd50
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-11 20:05:23 -0600

    understanding the turbo fish operator and its usecases. We are to use it to customize our expected output. for example as written, we can choose if we want the type to be output as an i8 vs an i32.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ fn identity<T>(value: T) -> T {
 struct Delisandwhich {}
 
 fn main() {
-    println!("{}", identity(5));
+    println!("{}", identity::<i32>(5));
+    println!("{}", identity::<i8>(5));
     println!("{}", identity(true));
     println!("{}", identity(String::from("Tacobell")));
     println!("{}", identity(2.234));
