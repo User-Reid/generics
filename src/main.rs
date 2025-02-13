5dbf3d6fc3a3e285ba60bab87c2400c77c39f3dd
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-12 20:52:33 -0600

    Understanding generics within enums

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,13 @@
-#[derive(Debug)]
-struct TreasureChest<T> {
-    captain: String,
-    treasure: T,
-}
-
-impl TreasureChest<String> {
-    fn clean_treasure_chest(&mut self) {
-        self.treasure = self.treasure.trim().to_string();
-    }
-}
-
-impl TreasureChest<[&str; 3]> {
-    fn amount_of_treasure(&self) -> usize {
-        self.treasure.len()
-    }
-}
-
-impl<T> TreasureChest<T> {
-    fn capital_captain(&self) -> String {
-        self.captain.to_uppercase()
-    }
+enum Cheesesteak<T> {
+    Plain,
+    Topping(T),
 }
 
 fn main() {
-    let gold_chest: TreasureChest<&str> = TreasureChest {
-        captain: String::from("Firebeard"),
-        treasure: "Gold",
-    };
-    println!("{:?}", gold_chest.capital_captain());
-
-    let mut silver_chest: TreasureChest<String> = TreasureChest {
-        captain: String::from("Mac"),
-        treasure: String::from("                Silver          "),
-    };
-    println!("{silver_chest:?}");
-    silver_chest.clean_treasure_chest();
-    println!("{silver_chest:?}");
-    println!("{:?}", silver_chest.capital_captain());
-
-    let special_chest: TreasureChest<[&str; 3]> = TreasureChest {
-        captain: String::from("Hook"),
-        treasure: ["Hello", "Hi", "Howdy"],
-    };
-    println!("{}", special_chest.amount_of_treasure());
-    println!("{special_chest:?}");
-    println!("{:?}", special_chest.capital_captain());
-}
+    let mushroom: Cheesesteak<&str> = Cheesesteak::Topping("Mushroom");
+    let onions: Cheeseseak<String> = Cheesesteak::Topping(String::from("Onions"));
+    let topping: String = String::from("Bacon");
+    let bacon: Cheesesteak<&String> = Cheesesteak(&topping);
+    
+    let mut plain: Cheesesteak<String> = Cheesesteak::Plain;
+    plain = Cheesesteak::Topping(String::from("Chicken"));
